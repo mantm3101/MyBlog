d5f8e4ec3ff8d5e7503debac9f85faffa032cb63
unknown <mantm3101@gmail.com>
2019-12-02 17:54:29 +0700

    Init admin and user layout

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('user.blogs.index');
+});
+
+Route::get('/blogs', function(){
+    return view('user.blogs.detail');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::group([ 'middleware' => 'auth', 'prefix' => 'admin' ], function(){
+
+    Route::get('/', function(){
+        return view('admin.layouts.main');
+    });
+    
+});
